69e036c493f6f72a7f8a85a1419369e1933bea4f
unknown <you@example.com>
2022-09-06 21:38:45 -0500

    debugged the 'return first' and 'return last' array

--- my_task1_code.js
+++ my_task1_code.js
@@ -2,60 +2,49 @@
 // - Given the following 6 empty functions, add the right return statement to each of them to complete the activity
 // Please do not change any of the function names
 
-//This is your starting array
 const arr = [10, 10, 16, 12];
 
 function returnFirst(arr) {
-    // return the first item from the array
     return arr[0];
 }
-// console.log('Result: ' + returnFirst(arr));
+console.log('Result: ' + returnFirst(arr));
 
 function returnLast(arr) {
-    // return the last item of the array
     return arr[3];
 }
-// console.log('Result: ' + returnLast(arr));
+console.log('Result: ' + returnLast(arr));
 
 function getArrayLength(arr) {
-    // return the length of the array
     return arr.length;
-    // console.log('Result: ' + getArrayLength(arr));
+    console.log('Result: ' + getArrayLength(arr));
 }
 
 function incrementByOne(arr) {
-    // arr is an array of integers(numbers), Increment all items in the array by
-    // return the array
     for (let i = 0; i < arr.length; i++) {
         arr[i] = arr[i] + 1;
         return arr;
     }
+    console.log('Result: ' + incrementByOne(arr));
 
-
-    function addItemToArray(arr, item) {
-        // add the parameter item to the end of the array arr
-        // return the array
-        arr.push(item);
+    function addItemToArray( arr, item ) {
+        arr.push ( item );
         return arr;
     }
-    // console.log('Result: ' + arr);
+    console.log ( 'Result: ' + addItemToArray(arr));
 
-    function addItemToFront(arr, item) {
-        // add the parameter item to the front of the array arr
-        // return the array
-        // hint: use the array method .unshift
-        arr.unshift(item);
+    function addItemToFront( arr, item ) {
+        arr.unshift ( item );
         return arr;
-        // console.log('Result: ' + arr);
+        console.log ( 'Result: ' + addItemToFront(arr) );
     }
 
 //uncomment these lines to check results in browser console
-    console.log("returnFirst result:" + returnFirst(arr))
-    console.log("returnLast result:" + returnLast(arr))
-    console.log("getArrayLength result:" + getArrayLength(arr))
-    console.log("incrementByOne result:" + incrementByOne(arr))
-    console.log("addItemToArray result:" + addItemToArray(arr, 10))
-    console.log("addItemToFront result:" + addItemToFront(arr, 10))
+    console.log ( "returnFirst result:" + returnFirst ( arr ) )
+    console.log ( "returnLast result:" + returnLast ( arr ) )
+    console.log ( "getArrayLength result:" + getArrayLength ( arr ) )
+    console.log ( "incrementByOne result:" + incrementByOne ( arr ) )
+    console.log ( "addItemToArray result:" + addItemToArray ( arr, 10 ) )
+    console.log ( "addItemToFront result:" + addItemToFront ( arr, 10 ) )
 
 //////////////////////////////////
 //don't change this line
@@ -69,3 +58,4 @@ function incrementByOne(arr) {
             addItemToFront,
         };
     }
+}
